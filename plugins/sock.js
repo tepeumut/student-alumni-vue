9f7bf58560b5af8b118106fddf8fd21537817ca4
Umut Tepe <tepeumut1@gmail.com>
2021-01-24 21:27:10 +0300

    Fixed SockJS

--- plugins/sock.js
+++ plugins/sock.js
@@ -1,7 +1,7 @@
 import SockJS from "sockjs-client";
 import "./stomp.min.js";
 export default (context, inject) => {
-  let $sockJS = new SockJS("http://localhost:8080/gs-guide-websocket");
+  let $sockJS = new SockJS("https://student-alumni-spring.herokuapp.com/gs-guide-websocket");
   let $stompClient = Stomp.over($sockJS);
   $stompClient.debug = () => {};
   inject('sockJS', $sockJS);
